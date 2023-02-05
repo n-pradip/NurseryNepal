2124e6c4b414e327cabbdfcc98586206c6b07582
n-pradip <n.pradip101@gmail.com>
2023-02-05 23:58:20 +0545

    refactor: Change design of blog and product card

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Image from '../images/bogunvellia.jpg'
 import ButtonPrimary from './ButtonPrimary'
 
 const BlogCard = () => {
     return (
-        <div>
-
-            <div class="w-52 sm:w-72 bg-white border border-gray-200 rounded shadow">
-                <a href="#">
-                    <img class="rounded-t-sm" src={Image} alt="" />
-                </a>
-                <div class="p-[10px]">
-                    <a href="#">
-                        <h5 class="mb-2 text-base font-semibold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 text-base font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021...</p>
-                    <ButtonPrimary text="Read more"/>
-                </div>
+        <>
+            <div className="w-60 sm:w-full bg-white border border-gray-200 rounded shadow p-[10px]">
+                <img className="rounded-t-sm object-cover" src={Image} alt="" />
+                <section className="py-[10px]">
+                    <Link to="/">
+                        <h5 className="mb-2 text-base font-semibold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+                    </Link>
+                    <p className="mb-3 text-base font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021...</p>
+                    <Link to="/" >
+                        <ButtonPrimary text="Read more" />
+                    </Link>
+                </section>
             </div>
-
-        </div>
+        </>
     )
 }
 
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import ButtonPrimary from '../components/ButtonPrimary'
+import ProductImage from '../images/bogunvellia.jpg'
+import { Link } from 'react-router-dom'
 
 const Card = () => {
     return (
         <div>
-            <div className="w-72 bg-white drop-shadow-lg rounded-sm">
-                <img className="object-cover rounded-tl-sm rounded-tr-sm"
-                    src="https://www.kindacode.com/wp-content/uploads/2022/06/computer-example.jpg" />
-                <div className="px-5 py-3 space-y-2">
-                    <a href="#"><h3 className="text-[18px] font font-semibold">Boguenvilla Flower(scientific name)</h3></a>
-                    <p className="space-x-2">
-                        <span className="text-[18px] font-semibold">Rs. 450</span>
-                        <span className="text-sm line-through text-gray-500">Rs. 1080</span>
-                        <span className="text-base text-red-700">58.33% off</span>
-                    </p>
+            <div className="w-60 sm:w-full p-[10px] drop-shadow-lg rounded-sm bg-white border-[1px] border-gray-200">
+                <img className="object-cover rounded-t-sm" src={ProductImage} />
+                <div className="py-2 space-y-2">
+                    <Link href="#"><h3 className="text-[16px] font font-semibold">Boguenvilla Flower (scientific name)</h3></Link>
+                    <div className='flex flex-wrap items-center justify-between'>
+                        <div className="product-card-selling-cost">
+                            <span className="text-[18px] font-semibold text-red-500">Rs. 450</span>
+                        </div>
+                        <div className="product-cardmarked-cost">
+                            <span className="ml-2 text-sm line-through text-gray-500">Rs. 1080</span>
+                            <span className="ml-2 text-base text-red-700">58.33% off</span>
+                        </div>
+                    </div>
                     <div className="flex justify-between items-center pt-3 pb-2">
-                        <ButtonPrimary text="Add to cart"/>
-                        <a href="#" title="Add to Favorites"
-                            className="text-2xl text-gray-300 hover:text-red-500 duration-300">&hearts;</a>
+                        <ButtonPrimary text="Add to cart" />
+                        <Link href="#" title="Add to Favorites" className="text-2xl text-gray-300 hover:text-red-500 duration-300"> &hearts; </Link>
                     </div>
                 </div>
             </div>
