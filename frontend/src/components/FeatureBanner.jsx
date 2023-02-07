f8b6a4c2c2bb5f67d24e9546dd42783e4307368b
n-pradip <n.pradip101@gmail.com>
2023-02-07 09:44:17 +0545

    feat: Update feature banner

--- frontend/src/components/FeatureBanner.jsx
+++ frontend/src/components/FeatureBanner.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { AiOutlineUser, AiOutlineHome } from "react-icons/ai";
-import { CiDeliveryTruck } from "react-icons/ci";
-import { BsClockHistory } from "react-icons/bs";
+import { CiDeliveryTruck, CiUser, CiTimer, CiHome } from "react-icons/ci";
 
 const FeatureBanner = () => {
   return (
     <div className='my-10 bg-[#F8F8F8] w-100'>
       <div className="container mx-auto px-10 flex flex-wrap justify-center py-5 text-center">
         <div className="w-1/2 sm:w-1/4 p-5 flex flex-col items-center text-lg text-[#4D4D4D] font-medium">
-          <AiOutlineHome size={40}/>
+          <CiHome size={40}/>
           All Nepal delivery
         </div>
         <div className="w-1/2 sm:w-1/4 p-5 flex flex-col items-center text-lg text-[#4D4D4D] font-medium">
@@ -16,11 +14,11 @@ const FeatureBanner = () => {
           Secure shipping
         </div>
         <div className="w-1/2 sm:w-1/4 p-5 flex flex-col items-center text-lg text-[#4D4D4D] font-medium">
-          <AiOutlineUser size={40}/>
+          <CiUser size={40}/>
           Lifetime Support
         </div>
         <div className="w-1/2 sm:w-1/4 p-5 flex flex-col items-center text-lg text-[#4D4D4D] font-medium">
-          <BsClockHistory size={40}/>
+          <CiTimer size={40}/>
           Track Order
         </div>
       </div>
